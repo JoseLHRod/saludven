bd16d31c62b850f875a7c66df912e75fcfdfc19e
Jose Hernandez <hernandezrjosel@gmail.com>
2020-01-16 13:38:53 -0400

    arreglos 15/01

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@
         if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Por favor, complete la informacion e intente de nuevo.";
             exit;
         }
 
@@ -22,32 +22,32 @@
         $recipient = "info@example.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "Nuevo contacto desde la web: $name";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "Nombre: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Phone: $subject\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content .= "Teléfono: $subject\n\n";
+        $email_content .= "Mensaje:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "De: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Gracias! Su mensaje ha sido enviado.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! Algo va mal y su mensaje no ha podido ser enviado.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ha ocurrido un problema con su mensaje, por favor intente de nuevo.";
     }
 
 ?>
